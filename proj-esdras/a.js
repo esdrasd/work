8e6856017bc3106c09d609b34cbe897cff08d6e4
esdrasd <esdrassousa76@gmail.com>
2020-01-30 00:32:32 -0300

    30/01/2020

--- proj-esdras/a.js
+++ proj-esdras/a.js
@@ -1,4 +1,5 @@
 var mysql = require('mysql');
+var Sequelize = require('sequelize');
 
 var con = mysql.createConnection({
 	host: "localhost",
@@ -6,17 +7,67 @@ var con = mysql.createConnection({
 	password: ""
 	});
 
-con.connect(function(err){	
+//conexao com banco de dados
+con.connect(function(err){
+	if(err){
+		console.log('- erros na conexao');
+	}else{
+		console.log('- conexao sucedida');
+	}
 });
 
-var db = 'bancox';
-var q = 'create database '+db;
-console.log(q);
+//testando query
+var nome_banco = 'bancox';
+var criando_banco = 'create database '+nome_banco;
+console.log('- teste query: '+criando_banco);
 
-con.query(q,function(err){
+//criando banco de dados
+con.query(criando_banco,function(err){
 	if(err){
-		console.log('erro')
+		usando_table();
 	}else{
-		console.log('connect')
+		console.log('- criado com sucesso banco de dados');
+		criando_table();
 	}
-})
+});
+
+//use banco
+const sequelize = new Sequelize(nome_banco,'root','',{
+  host:'localhost',
+  dialect:'mysql'
+});
+
+//criando table
+var criando_table = function(){
+	const nome_table = 'USUARIOS';
+	const usuario = sequelize.define(nome_table,{
+	  nome:{
+	    type:Sequelize.STRING
+	  },
+	  senha:{
+	    type:Sequelize.STRING
+	  },
+	  telefone:{
+	    type:Sequelize.STRING
+	  }
+	});
+	usuario.sync({force:true});
+	console.log('- criando table: '+nome_table);
+}
+
+//usando table
+var usando_table = function(){
+	const nome_table = 'USUARIOS';
+	const usuario = sequelize.define(nome_table,{
+	  nome:{
+	    type:Sequelize.STRING
+	  },
+	  senha:{
+	    type:Sequelize.STRING
+	  },
+	  telefone:{
+	    type:Sequelize.STRING
+	  }
+	});
+	console.log('- usando table: '+nome_table);
+}
--- proj-esdras/connect.js
+++ proj-esdras/connect.js
@@ -1,36 +1,73 @@
-const mysql = require('mysql');
-const Sequelize = require('sequelize');
+var mysql = require('mysql');
+var Sequelize = require('sequelize');
 
-//connect Database
-const con = mysql.createConnection({
-   host: "localhost",
-   user: "root",
-   password:""
+var con = mysql.createConnection({
+	host: "localhost",
+	user: "root",
+	password: ""
+	});
+
+//conexao com banco de dados
+con.connect(function(err){
+	if(err){
+		console.log('- erros na conexao');
+	}else{
+		console.log('- conexao sucedida');
+	}
 });
 
-const n_banco = 'banco';
-const sequelize = new Sequelize(n_banco,'root','',{
+//testando query
+var nome_banco = 'bancox';
+var criando_banco = 'create database '+nome_banco;
+console.log('- teste query: '+criando_banco);
+
+//criando banco de dados
+con.query(criando_banco,function(err){
+	if(err){
+		usando_table();
+	}else{
+		console.log('- criado com sucesso banco de dados');
+		criando_table();
+	}
+});
+
+//use banco
+const sequelize = new Sequelize(nome_banco,'root','',{
   host:'localhost',
   dialect:'mysql'
 });
 
-const qqq_criar_tabela = 'USUARIOS';
-const usuario = sequelize.define(qqq_criar_tabela,{
-  nome:{
-    type:Sequelize.STRING
-  },
-  senha:{
-    type:Sequelize.STRING
-  },
-  telefone:{
-    type:Sequelize.STRING
-  }
-});
+//criando table
+var criando_table = function(){
+	const nome_table = 'USUARIOS';
+	const usuario = sequelize.define(nome_table,{
+	  nome:{
+	    type:Sequelize.STRING
+	  },
+	  senha:{
+	    type:Sequelize.STRING
+	  },
+	  telefone:{
+	    type:Sequelize.STRING
+	  }
+	});
+	usuario.sync({force:true});
+	console.log('- criando table: '+nome_table);
+}
 
-//usuario.sync({force:true});
-
-module.exports={
-  Sequelize: Sequelize,
-  sequelize: sequelize,
-  usuario: usuario
+//usando table
+var usando_table = function(){
+	const nome_table = 'USUARIOS';
+	const usuario = sequelize.define(nome_table,{
+	  nome:{
+	    type:Sequelize.STRING
+	  },
+	  senha:{
+	    type:Sequelize.STRING
+	  },
+	  telefone:{
+	    type:Sequelize.STRING
+	  }
+	});
+	console.log('- usando table: '+nome_table);
 }
